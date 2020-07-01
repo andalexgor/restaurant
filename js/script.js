7ab2670da378ec5e22820ae7186fc5a06e7825d3
ANDREY <aaazzznokia@gmail.com>
2020-07-01 21:54:08 +0400

    create 5 block, add form and slick slider

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.slick-slider-specialties').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    dots:true,
+    arrows:false,
+    autoplaySpeed: 3000,
+    touchMove:false,
+  });
