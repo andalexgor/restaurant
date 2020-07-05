dd32b6d947f5c18632d5a472e872cc2f2dd9a2db
ANDREY <aaazzznokia@gmail.com>
2020-07-05 13:49:01 +0400

    end of block creation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 $('.slick-slider-specialties').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    dots:true,
-    arrows:false,
-    autoplaySpeed: 3000,
-    touchMove:false,
-  });
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  dots: true,
+  arrows: false,
+  autoplaySpeed: 5000,
+  touchMove: false,
+  fade:true,
+
+});
