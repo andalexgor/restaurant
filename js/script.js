f2f7dc428c5834b6879ab09f7112f14026768fce
ANDREY <aaazzznokia@gmail.com>
2020-07-09 13:22:25 +0400

    add fixed mobile menu

--- js/script.js
+++ js/script.js
@@ -12,8 +12,10 @@ $('.slick-slider-specialties').slick({
 
 let toggleBurger = document.querySelector('.header__burger')
 let mobMenu=document.querySelector('.m-menu')
+let bodyLock=document.getElementsByTagName('body')[0]
 
   toggleBurger.addEventListener('click',(event)=>{
     const target=event.target
     mobMenu.classList.toggle('active-burger')
+    bodyLock.classList.toggle('lock')
   })
