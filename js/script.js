debc53692332251766c38e43cd4fa32659992c5b
ANDREY <aaazzznokia@gmail.com>
2020-07-13 14:06:59 +0400

    corrections for mobile version

--- js/script.js
+++ js/script.js
@@ -4,20 +4,24 @@ $('.slick-slider-specialties').slick({
   autoplay: true,
   dots: true,
   arrows: false,
-  autoplaySpeed: 50000,
+  autoplaySpeed: 5000,
   touchMove: false,
   fade: true,
   // adaptiveHeight: true,
 });
 
+
+
 let toggleBurger = document.querySelector('.header__burger')
 let mobMenu = document.querySelector('.m-menu')
-let bodyLock = document.getElementsByTagName('body')[0]
+let bodyLock = document.getElementsByTagName('html')[0]
+
 
 toggleBurger.addEventListener('click', (event) => {
   const target = event.target
   mobMenu.classList.toggle('active-burger')
   bodyLock.classList.toggle('lock')
+  toggleBurger.classList.toggle('active-span')
 })
 
 
@@ -26,47 +30,26 @@ for (let i = 0; i < mobMenuItems.length; i++) {
   mobMenuItems[i].onclick = function () {
     mobMenu.classList.toggle('active-burger')
     bodyLock.classList.toggle('lock')
+    toggleBurger.classList.toggle('active-span')
 
   }
 }
 
 let allFoodItems = document.querySelectorAll('.dish__desriptions')
-
-
-
 let foodCategory = document.querySelector('.categories-menu')
 foodCategory.addEventListener('click', event => {
 
-
-
- 
-
-
-
-
-
-
   if (event.target.tagName !== 'LI') return false;
   let filterClass = event.target.dataset['category']
-
   allFoodItems.forEach(elem => {
     elem.classList.remove('hide')
     if (!elem.classList.contains(filterClass) && filterClass !== 'all') {
       elem.classList.add('hide')
-
     }
-
-
   })
-
 })
-
-
-
 let deliciousMenuName = document.querySelectorAll('.delicious-menu-name')
 
-
-  
 for (let i = 0; i < deliciousMenuName.length; i++){
  
 deliciousMenuName[i].onclick = function() {
@@ -78,11 +61,3 @@ deliciousMenuName[i].onclick = function() {
 
 }
 
-// let mobMenuItems = document.querySelectorAll('.m-menu-burger-item')
-// for (let i = 0; i < mobMenuItems.length; i++) {
-//   mobMenuItems[i].onclick = function () {
-//     mobMenu.classList.toggle('active-burger')
-//     bodyLock.classList.toggle('lock')
-
-//   }
-// }
